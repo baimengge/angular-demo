3d16dcd4324fccc547e54e00c5d9207fb2452baf
baimengge <baimengge@rongyitech.com>
2017-03-03 10:27:38 +0800

    图片轮播

--- myapp/www/js/controller/homeCtrl.js
+++ myapp/www/js/controller/homeCtrl.js
@@ -2,5 +2,17 @@
 	angular.module('starter')//载入创建的模块
 		.controller('homeCtrl', ['$scope', function($scope){
 			console.log('hello world');
+			// 设立一个命名空间
+			$scope.slideData=[
+				{
+					img:'./img/a.jpg'
+				},
+				{
+					img:'./img/b.jpg'
+				},
+				{
+					img:'./img/timg.jpeg'
+				}
+			]
 		}])
 })(angular);
